271ec85d290029561f973b92415af611b3352928
Noixas\aCmRz <acmrz@outlook.com>
2018-03-17 12:11:44 +0100

    libGDX added

---
+++ libGDX/desktop/src/com/crazy_putting/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.crazy_putting.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crazy_putting.game.CrazyPutting;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrazyPutting(), config);
+	}
+}
