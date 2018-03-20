599cf55443e4b07b727b56174480245496577bc9
SimonOsadchiy <sposadchii@gmail.com>
2018-03-20 14:55:52 +0100

    debugged for the user input to be "0,0". Now if 0,0 is the input,then the ball stops and behaves due to the nature state

--- libGDX/core/src/com/crazy_putting/game/GameObjects/Ball.java
+++ libGDX/core/src/com/crazy_putting/game/GameObjects/Ball.java
@@ -65,8 +65,6 @@ public class Ball extends GameObject{
         return this.MASS;
     }
 
-
-
     public void update(float  dt){
        // System.out.println(getPosition());
     }
@@ -78,10 +76,15 @@ public class Ball extends GameObject{
         }
         if(input.getText()!=null){
             try{
+
                 String[] data = input.getText().split(" ");
-                setVelocity(Float.parseFloat(data[0]),Float.parseFloat(data[1]));
-                //velocity.speed = Float.parseFloat(data[0]);
-                //velocity.angle = Float.parseFloat(data[1]);
+                if(Float.parseFloat((data[0]))!=0) {
+                    setVelocity(Float.parseFloat(data[0]), Float.parseFloat(data[1]));
+                }
+                else{
+                    float e = (float) 0.0001;
+                    setVelocity(e,Float.parseFloat(data[1]));
+                }
                input.clearText();
             }
             catch(NumberFormatException e){
